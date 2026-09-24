76c3d457f1397cec50e782a3d1c02cb06cd24765
afiquewebappick <afique.webappick@gmail.com>
2026-09-24 16:19:13 +0600

    feat: add Sheet and Sidebar components for improved UI layout
    - Introduced Sheet component for modal-like functionality with customizable content, header, footer, and close button.
    - Implemented Sidebar component with context management for state handling, mobile responsiveness, and keyboard shortcuts.
    - Added Skeleton component for loading states and Table component for structured data display.
    - Created Tabs component for tabbed navigation and Tooltip component for contextual hints.
    - Developed useIsMobile hook to determine mobile view and added routes for admin, doctor, and patient sections.
    - Defined SidebarItem and SidebarGroup types for better type safety in sidebar navigation.
    - Added UserRole type for user role management.

--- src/components/layout/public/Header.tsx
+++ src/components/layout/public/Header.tsx
@@ -3,6 +3,7 @@
 import Logo from "@/assets/svg/Logo";
 import { Button } from "@/components/ui/button";
 import { useGetMe, useLogout } from "@/hooks";
+import { UserRole } from "@/types";
 import { useQueryClient } from "@tanstack/react-query";
 import Link from "next/link";
 import { toast } from "sonner";
@@ -13,9 +14,18 @@ export default function Header() {
     { name: "About us", url: "/about-us" },
   ];
 
+  const dashboardRoute: Record<UserRole, string> = {
+    SUPER_ADMIN: "/admin",
+    ADMIN: "/admin",
+    DOCTOR: "/doctor",
+    PATIENT: "/patient",
+  };
+
   const { data, isLoading } = useGetMe();
   const { mutate: logout } = useLogout();
   const queryClient = useQueryClient();
+
+  const role: UserRole = data?.data && data?.data.role;
 
   const handleLogout = () => {
     logout(undefined, {
@@ -47,6 +57,8 @@ export default function Header() {
               {route.name}
             </Link>
           ))}
+
+          {role && <Link href={dashboardRoute[role]}>Dashboard</Link>}
         </nav>
         <div>
           {!isLoading && !data && (
---
+++ src/routes/admin.routes.ts
@@ -0,0 +1,3 @@
+const prefix = "/admin";
+
+export const adminRoutes = []
